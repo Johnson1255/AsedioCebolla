67241f0f6c2dcbc2f701ec2d4de59ce8a7f4398d
Johnson1255 <johnson.chen@upb.edu.co>
2023-11-13 20:02:24 -0500

    Primer Ajuste de Codigos

--- Assets/Oleada/Oleadas.cs
+++ Assets/Oleada/Oleadas.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Tools;
 
 public class Oleadas : MonoBehaviour
 {
+    [SerializeField]
+    private List<Ola> olas;
+    private int ola_actual;
+    [SerializeField]
+    private GameObject contenedor_grupos;
     public static Oleadas Instance { get; private set; }
-    
+    public GameObject ContenedorGrupos { get => contenedor_grupos; set => contenedor_grupos = value; }
+    public int OlaActual { get => ola_actual; set => ola_actual = value; }
+
+    public int CantidadOlas()
+    {
+        return olas.Count;
+    }
+
     private void Awake() 
     { 
         // If there is an instance, and it's not me, delete myself.
@@ -21,11 +34,24 @@ public class Oleadas : MonoBehaviour
     }
 
     public void Start(){
-        //Debug.Log("Se asignan los caminos");
+        DespacharOla();
     }
 
-    public Enemigo BuscarEnemigoDisponible(TipoEnemigo tipo)
-    {
-        return null;
+    public void DespacharOla(){
+        Ola ola;
+        if(ola_actual < olas.Count)
+        {
+            ola = olas[ola_actual];
+            ola.EmpezarOla();
+            ola.OleadasNivel = this;
+            ola_actual++; //Acá se acualiza el contador de olas
+            MMGameEvent.Trigger("ActualizarUIOla");
+        }
+        else{
+            //Se termina la partida indicar que gano y mostrar ventana fin de nivel
+            Debug.Log("Fin de oleadas");
+        }
+
     }
+
 }
